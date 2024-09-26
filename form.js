ea8a4390861b56e9fe1f6352c7839da72271aab6
OwaisQureshi123 <76607219+OwaisQureshi123@users.noreply.github.com>
2024-09-26 21:45:27 +0800

    Add files via upload

--- form.js
+++ form.js
@@ -3,7 +3,17 @@ $(function()
 {
     function after_form_submitted(data) 
     {
-        if(data.result == 'success')
+
+        // alert("HELLO123");
+        // alert(data.status)
+
+        // if(xhr.readyState == 4 && xhr.status == 200) {
+        //     // alert(data.responseText);
+        //     //alert("AWESOME PEOPLE");
+        // }
+
+
+        if(data.status == 201 || data.status == 200)
         {
             $('#success_message').show();
             $('#error_message').hide();
@@ -35,28 +45,125 @@ $(function()
         }//else
     }
 
-	$('#contact_form').submit(function(e)
-      {
+	$('#contact_form').submit(function(e){
+        
         e.preventDefault();
 
         $form = $(this);
+
         //show some response on the button
-        $('button[type="submit"]', $form).each(function()
-        {
+        $('button[type="submit"]', $form).each(function(){
             $btn = $(this);
             $btn.prop('type','button' ); 
             $btn.prop('orig_label',$btn.text());
             $btn.text('Sending ...');
         });
-        
 
-                    $.ajax({
-                type: "POST",
-                url: 'handler.php',
-                data: $form.serialize(),
-                success: after_form_submitted,
-                dataType: 'json' 
-            });        
+        // Example URL with query parameters
+        const url = $form.serialize();
+
+        // Create a URL object
+        const urlObj = new URLSearchParams(url);
+
+        // Use URLSearchParams to get query parameters
+        const responseParams = new URLSearchParams(urlObj);
+
+        // Convert query parameters to a JSON object
+        const jsonParams = {};
+        responseParams.forEach((value, key) => {
+            jsonParams[key] = value;
+        });
+
+        // Convert the JSON object to a string (optional)
+        // const jsonString = JSON.stringify(jsonParams);
+
+        // Log the JSON object and string
+
+        var patientName = jsonParams["Name"];
+        var patientEmail = jsonParams["Email"];
+        var patientMessage = jsonParams["message"];
+        var patientPhone = jsonParams["phone"];
+        var patientService = jsonParams["service"];
+        var patientTime = jsonParams["time"];
+
+        var params = JSON.stringify({
+            // "sender":{
+            //     "email":"umerkhattab42@gmail.com",
+            //     "name":"Owais"
+            // },
+            "subject": `Appointment Request: ${patientService}`,
+            "templateId":1,
+            "params":{
+
+                "patient_service": patientService,
+                "patient_name":  patientName,
+                "patient_email": patientEmail,
+                "patient_message" : patientMessage,
+                "patient_phone" : patientPhone,
+                "patient_time" : patientTime
+            },
+            "messageVersions":[
+                
+                //Definition for Message Version 1 
+                {
+                    "to":[
+                        {
+                        "email":"bob@example.com",
+                        "name":"Bob Anderson"
+                        },
+                        {
+                        "email":"anne@example.com",
+                        "name":"Anne Smith"
+                        }
+                    ],
+                    "params":{
+                        "greeting":"Welcome onboard!",
+                        "headline":"Be Ready for Takeoff."
+                    },
+                    "subject":"We are happy to be working with you"
+                },
+                
+                // Definition for Message Version 2
+                {
+                    "to":[
+                        {
+                        "email":"umerkhattab42@gmail.com",
+                        "name":"Owais"
+                        },
+                        {
+                        "email":"umer_khattab@live.com",
+                        "name":"Mark Payton"
+                        },
+                        // {
+                        // "email":"andrea@example.com",
+                        // "name":"Andrea Wallace"
+                        // }
+                    ],
+                    "params":{
+                        "greeting":"Hello there..."
+                    }
+                }
+            ]
+            });
+
+            const xhr = new XMLHttpRequest();
+                  xhr.open("POST", "https://api.brevo.com/v3/smtp/email");
+                  xhr.setRequestHeader("accept", "application/json")
+                  xhr.setRequestHeader("api-key", "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")
+                  xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
+                  xhr.onreadystatechange = function() {
+                        after_form_submitted(xhr);   
+                  }
+
+                  xhr.send(params);
+
+        // $.ajax({
+        //         type: "POST",
+        //         url: 'https://api.brevo.com/v3/smtp/email',
+        //         data: $form.serialize(),
+        //         success: after_form_submitted,
+        //         dataType: 'json' 
+        //     });        
         
       });	
 });
---
+++ js/services-content.js
@@ -0,0 +1,43 @@
+
+let serviceContent = [
+    {
+        
+    },
+    {
+        "name" : "Individual Therapy",
+        "img" : "images/services/1.webp",
+        "content" : "",
+        "price" : "$145 - $150",
+        "short_desc" : "One-on-one counseling process where a person works with a mental health professional to address personal emotional, psychological, or behavioral issues."
+    },
+    {
+        "name" : "Couples and Family Counseling",
+        "img" : "images/services/2.webp",
+        "content" : "",
+        "price" : "$175 - $185",
+        "short_desc" : "A therapeutic process where a professional helps partners address and resolve relationship issues to improve communication and strengthen their bond."
+    },
+    {
+        "name" : "Stress Management",
+        "img" : "images/services/4.webp",
+        "content" : "",
+        "price" : "N/A",
+        "short_desc" : "A process of using techniques and strategies to identify, reduce, and cope with stress in order to maintain overall well-being."
+    },
+    {
+        "name" : "Anxiety Management",
+        "img" : "images/services/5.webp",
+        "content" : "",
+        "price" : "N/A",
+        "short_desc" : "Use of various strategies and techniques to identify, address, and reduce anxiety symptoms, helping individuals maintain a sense of calm and improve their overall well-being."
+    },
+    {
+        "name" : "Depression and Trauma Therapy",
+        "img" : "images/services/6.webp",
+        "content" : "",
+        "price" : "N/A",
+        "short_desc" : "Helping individuals alleviate symptoms of depression and improve mood, while trauma therapy supports healing from traumatic experiences and reduces associated symptoms."
+
+    },
+
+];
